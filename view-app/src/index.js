337ae21d2d7edb00d1ee921b6ba6b3067e979db0
Youssefares <Youssefares@ymail.com>
2017-11-12 17:13:17 +0200

    Fix tests and modify generated index.js file to ignore eslint

--- view-app/src/index.js
+++ view-app/src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,6 +11,6 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 registerServiceWorker();
