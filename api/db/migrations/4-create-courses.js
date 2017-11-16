a6b6a5e10609dcf827587a6163157f8608a40779
Youssefares <Youssefares@ymail.com>
2017-11-16 19:11:28 +0200

    Add courses migrations

---
+++ api/db/migrations/4-create-courses.js
@@ -0,0 +1,22 @@
+const Promise = require('bluebird');
+const getConnection = require('../connection');
+
+const createCoursesSQL =
+'CREATE TABLE courses (\
+  id int AUTO_INCREMENT,\
+  department_id int NOT NULL,\
+  name varchar(255) NOT NULL,\
+  description varchar(255),\
+  credit_hours int,\
+  instructor_name varchar(255),\
+  PRIMARY KEY (id),\
+  FOREIGN KEY (department_id) REFERENCES departments(id)\
+)';
+
+
+Promise.using(getConnection(), (connection) => {
+  connection.query(createCoursesSQL);
+}).then(() => {
+  console.log('table coursess created successfully');
+  process.exit(0);
+});
